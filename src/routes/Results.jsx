8b41c7db879b50352f41b88c9ebd081601fb5678
Kobe Vervaele <kobe.vervaele2@gmail.com>
2023-04-11 20:54:36 +0200

    kijk results git push origin main

--- src/routes/Results.jsx
+++ src/routes/Results.jsx
@@ -58,6 +58,60 @@ export default function Home() {
                             </li>
                         </ol>
                     </nav>
+                    <div className="text-center mt-6">
+                        <h1 className="text-2xl font-semibold text-white capitalize lg:text-3xl dark:text-white">
+                            Results
+                        </h1>
+
+                    <p className="max-w-lg mx-auto mt-4 text-white">
+                        Here you can view your results
+                    </p>
+                    </div>
+                        <div className="relative top-10">
+                        <a href="" className="group static block h-80 w-80">
+                            <span className="inset-0"></span>
+
+                            <div
+                                className="relative flex h-full transform items-end bg-white transition-transform group-hover:-translate-x-2 group-hover:-translate-y-2"
+                            >
+                                <div
+                                className="p-4 !pt-0 transition-opacity group-hover:absolute group-hover:opacity-0 sm:p-6 lg:p-8"
+                                >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-10 w-10 sm:h-12 sm:w-12"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                    />
+                                </svg>
+
+                                <h2 className="mt-4 text-xl font-medium sm:text-2xl">Titel / Leerkracht</h2>
+                                </div>
+
+                                <div
+                                className="absolute p-4 opacity-0 transition-opacity group-hover:relative group-hover:opacity-100 sm:p-6 lg:p-8"
+                                >
+                                <h3 className="mt-4 text-xl font-medium sm:text-2xl">Titel / Leerkracht</h3>
+
+                                <p className="mt-4 text-sm sm:text-base">
+                                    Feedback op je resultaten van de leerkracht die horen bij deze evaluatie.
+                                </p>
+
+                                <p className="mt-8 font-bold">Lees meer</p>
+                                </div>
+                            </div>
+                            </a>
+
+                        </div>
+
+                        
                 </div>
             </div>
         </div>
