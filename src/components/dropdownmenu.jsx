65f4a470561f2a57327e3682f4df06b221f4f42a
Kobe Vervaele <kobe.vervaele2@gmail.com>
2023-04-27 11:01:07 +0200

    Miauw

---
+++ src/components/dropdownmenu.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+
+const jaren = [
+  { name: "Jaar 1", klassen: [{ klas: "1A" }, { klas: "1B" }, { klas: "1C" }] },
+  { name: "Jaar 2", klassen: [{ klas: "2A" }, { klas: "2B" }, { klas: "2C" }] },
+  { name: "Jaar 3", klassen: [{ klas: "3A" }, { klas: "3B" }, { klas: "3C" }] },
+  { name: "Jaar 4", klassen: [{ klas: "4A" }, { klas: "4B" }, { klas: "4C" }] },
+  { name: "Jaar 5", klassen: [{ klas: "5A" }, { klas: "5B" }, { klas: "5C" }] },
+  { name: "Jaar 6", klassen: [{ klas: "6A" }, { klas: "6B" }, { klas: "6C" }] },
+];
+
+const DropdownMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+    setIsOpen1(false);
+  };
+
+  const [selectedJaar, setSelectedJaar] = useState(null);
+  const [isOpen1, setIsOpen1] = useState(false);
+  const [selectedKlas, setSelectedKlas] = useState(null);
+
+  const handleJaarClick = (jaar) => {
+    setSelectedJaar(jaar);
+    setIsOpen1(!isOpen1);
+  };
+
+  const handleKlasClick = (klas) => {
+    setSelectedKlas(klas);
+    setIsOpen(false);
+    setIsOpen1(false);
+  };
+
+  const klassen =
+    selectedJaar && selectedJaar.klassen ? selectedJaar.klassen : [];
+
+  const klasButtonText = selectedKlas ? selectedKlas.klas : "Klas";
+
+  return (
+    <div className="relative">
+      <button
+        className="w-16 rounded-md bg-slate-800 px-3 py-2 shadow-sm focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm text-gray-200"
+        onClick={toggleDropdown}
+      >
+        {klasButtonText}
+      </button>
+
+      {isOpen && (
+        <div className="absolute top-0 left-0 bg-slate-800 mt-10 py-2 w-48 shadow-lg rounded-lg">
+          {jaren.map((jaar) => (
+            <button
+              className="block px-4 py-2 text-sm text-gray-200 hover:bg-gray-700 w-full text-left"
+              key={jaar.name}
+              onClick={() => handleJaarClick(jaar)}
+            >
+              {jaar.name}
+            </button>
+          ))}
+        </div>
+      )}
+
+      {isOpen1 && (
+        <div className="absolute mt-10 top-0 left-0 ml-52 bg-slate-800 py-2 w-48 shadow-lg rounded-lg">
+          {klassen.map((klas) => (
+            <button
+              className="block px-4 py-2 text-sm text-gray-200 hover:bg-gray-700 w-full text-left"
+              key={klas.klas}
+              selectedKlas={klas}
+              onClick={() => handleKlasClick(klas)}
+            >
+              {klas.klas}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DropdownMenu;
--- src/routes/SettingsAccount.jsx
+++ src/routes/SettingsAccount.jsx
@@ -3,32 +3,75 @@ import Header from "../components/header";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase";
 import Sidebar from "../components/sidebar";
+import DropdownMenu from "../components/dropdownmenu";
 
 export default function Home() {
-    const [loggedIn, setLoggedIn] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
 
-    useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                // User signed in
-                setLoggedIn(true);
-            } else {
-                window.location.replace("/not-logged-in");
-            }
-        });
-    }, []);
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User signed in
+        setLoggedIn(true);
+      } else {
+        window.location.replace("/not-logged-in");
+      }
+    });
+  }, []);
 
-    return (
-        <div className="w-screen h-screen bg-slate-700 overflow-hidden">
-            <Header />
-            <div className="flex">
-                <div className="max-w-xs">
-                    <Sidebar />
-                </div>
-                <div className="p-8 w-full">
-                    
-                </div>
-            </div>
+  return (
+    <div className="w-screen h-screen bg-slate-700 overflow-hidden">
+      <Header />
+      <div className="flex">
+        <div className="sidebar max-w-xs w-1/5 bg-slate-700">
+          <Sidebar />
         </div>
-    );
+        <div className="w-4/5 mx-auto">
+          <div className="content flex flex-col w-4/5">
+            <div className="flex items-center">
+              <div className="p-8 w-2/5">
+                <label for="UserEmail" class="text-white">
+                  Geef hier je nieuwe E-mail in
+                </label>
+                <input
+                  id="UserEmail"
+                  placeholder="kobe.vervaele2@gmail.com"
+                  class="w-full rounded-md bg-slate-800 px-3 py-2 shadow-sm focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm text-gray-200"
+                />
+              </div>
+              <div className="p-8 w-2/5">
+                <label for="UserEmail" class="text-white">
+                  Geef hier je klas is
+                </label>
+                <DropdownMenu />
+              </div>
+            </div>
+
+            <div className="p-8 w-2/5">
+              <label for="UserName" class="text-white">
+                Geef hier je voornaam in
+              </label>
+              <input
+                id="UserName"
+                placeholder="naam"
+                class=" w-full rounded-md bg-slate-800 px-3 py-2 shadow-sm focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm text-gray-200"
+              />
+
+              <label for="UserLName" class="text-white ">
+                Geef hier je achternaam in
+              </label>
+              <input
+                id="UserLName"
+                placeholder="familie naam"
+                class=" w-full rounded-md bg-slate-800 px-3 py-2 shadow-sm focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm text-gray-200"
+              />
+              <button className="bg-blue-700 text-white py-2 px-5 rounded-full font-semibold ml-1 hover:bg-blue-600 w-32 b-0 l-0 mt-11">
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
